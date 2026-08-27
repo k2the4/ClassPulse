76e9092450d6407587a4f83dc3d4a4f9b6fb1831
k2the4 <kvasisth2000@gmail.com>
2026-08-27 19:37:16 +0530

    Load shared analysis visual system in Pages Router

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import GlobalButtonLoading from "../components/GlobalButtonLoading";
 import "../styles/globals.css";
+import "../app/globals.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
