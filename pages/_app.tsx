7c05d2aea0cfe0d0976bd843fad002eb421ed0f6
k2the4 <kvasisth2000@gmail.com>
2026-08-29 10:20:11 +0530

    Apply Overall Analysis layout fixes globally

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import { SessionProvider } from "next-auth/react";
 import GlobalButtonLoading from "../components/GlobalButtonLoading";
 import "../styles/globals.css";
 import "../app/globals.css";
+import "../styles/overall-analysis-fixes.css";
 
 export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
